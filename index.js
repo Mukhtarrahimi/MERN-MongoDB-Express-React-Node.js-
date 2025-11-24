bd006361416836de05dfdf7b1b3bfe9b8bf463f6
= <mukhtarrahimi.dev@gmail.com>
2025-11-24 19:52:39 +0430

    server runngin code

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ dotenv.config();
 import connectDb from './config/db.js';
 app.use(express.json());
 app.use(morgan('dev'));
-connectDb();
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+// server running
+const startServer = async () => {
+  try {
+    await connectDb();
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`server runnging on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(`Failed to start server ${err.message}`);
+    process.exit();
+  }
+};
+
+startServer();
