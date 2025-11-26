4b7a717d04b3a253044a971e79fce16ddd677415
= <mukhtarrahimi.dev@gmail.com>
2025-11-26 21:13:25 +0430

    update user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,9 +4,11 @@ import {
   createUser,
   getAllUser,
   getUser,
+  updateUser,
 } from '../controllers/userController.js';
 
 router.post('/create', createUser);
 router.get('/', getAllUser);
 router.get('/:id', getUser);
+router.put('/:id', updateUser);
 export default router;
